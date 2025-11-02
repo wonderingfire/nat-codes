69fbbb642334a592bb7a9dd05634de10ac70df3a
wonderingfire <nathanielchavez.codes@gmail.com>
2025-11-02 10:36:50 +0800

    Added basic hero and header components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Hero } from "./components/hero/hero";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  logoText = '<NC />';
   protected readonly title = signal('nat-codes');
 }
